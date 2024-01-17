26dbf5ec1247c12835c46734838dca92e547f5c5
waldix04 <152854401+waldix04@users.noreply.github.com>
2024-01-17 17:32:47 +0100

    Delete Programmieren/Unterricht directory

--- Programmieren/Unterricht/muster.py
+++
@@ -1,8 +0,0 @@
-def dreieck_muster():
-    for i in range(1, 7):
-        print('x' * i)
-
-    for i in range(5, 0, -1):
-        print('x' * i)
-
-dreieck_muster()
--- Programmieren/Unterricht/schere_stein_papier.py
+++
@@ -1,2 +0,0 @@
-import random 
-user_action = input("Enter a choice (rock,papers,scissors)")
